7b410c99eefe186ee2b2b36c57b74b2973972982
otuekong <myshield02@gmail.com>
2025-10-30 16:45:39 +0100

    updated with docstring and some comments

--- stage2/src/config.py
+++ stage2/src/config.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    """
+    Configuration settings for the Flask application.
+    """
     PORT = int(os.environ.get("PORT", 5000))
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "mysql+pymysql://hng13:stage2@db:3306/country_currency_exchange")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     REFRESH_TIMEOUT_SECONDS = int(os.environ.get("REFRESH_TIMEOUT_SECONDS", 30))
