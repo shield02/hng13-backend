5a8ccea04cdece76aeb2f5825b3b32a35bea1ca2
otuekong <myshield02@gmail.com>
2025-10-30 14:54:23 +0100

    updated the tables

--- stage2/src/models.py
+++ stage2/src/models.py
@@ -1,19 +1,23 @@
-from database import db
+from .database import db
+from datetime import datetime, timezone
+
 
 class Country(db.Model):
-    __tablename__ = "countries"
+    __tablename__ = 'countries'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False, unique=True)
-    capital = db.Column(db.String(255), nullable=True)
-    region = db.Column(db.String, nullable=True)
-    population = db.Column(db.Integer, nullable=True)
-    currency_code = db.Column(db.String(10), nullable=True)
-    exchange_rate = db.Column(db.Float, nullable=True)
-    estimated_gdp = db.Column(db.Float, nullable=True)
-    flag_url = db.Column(db.Text, nullable=True)
-    last_refreshed_at = db.Column(db.DateTime, nullable=True)
+    name = db.Column(db.String(128), nullable=False)
+    capital = db.Column(db.String(128))
+    region = db.Column(db.String(64))
+    population = db.Column(db.BigInteger)
+    currency_code = db.Column(db.String(8), nullable=False)
+    exchange_rate = db.Column(db.Float)
+    estimated_gdp = db.Column(db.Float)
+    flag_url = db.Column(db.String(255))
+    last_refreshed_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
 class Meta(db.Model):
-    __tablename__ = "meta"
-    key = db.Column(db.String(100), primary_key=True)
-    value = db.Column(db.Text)
+    __tablename__ = 'meta'
+
+    id = db.Column(db.Integer, primary_key=True)
+    last_refreshed_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
